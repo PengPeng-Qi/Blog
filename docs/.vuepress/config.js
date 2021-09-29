016078734e62546d79b473138365530e131dd909
PengPengQ <57583682+PengPeng-Qi@users.noreply.github.com>
2021-09-30 00:06:38 +0800

    打包测试4.0

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
   themeConfig: {
     navbar: [{
         text: '笔记',
-        link: '/notes/',
+        link: '/notes/HTML/',
       },
       {
         text: '项目',
@@ -33,31 +33,18 @@ module.exports = {
           link: '/notes/React/',
         }, {
           text: '数据结构',
-          link: '/notes/数据结构/',
+          link: '/notes/dataTree/',
         },
         {
           text: '其他',
-          children: [{
-            text: 'echo 命令',
-            link: '/notes/Others/echo',
-          }, {
-            text: '浅谈深浅拷贝',
-            link: '/notes/Others/深浅拷贝',
-          }],
-        }
+          link: '/notes/Others/',
+        },
       ],
       '/projects/': [{
         text: 'Reference',
       }, ],
     },
     editLink: true,
-    /*     
-      editLinkText: '如果发现有误，可以点我进行编辑哦',
-      docsRepo: 'https://github.com/PengPeng-Qi',
-      docsBranch: 'master',
-      docsDir: 'docs',
-      editLinkPattern: ':repo/-/edit/:branch/:path',
-    */
     logo: '/images/favicon.png',
     backToHome: '别走丢了哦，点我回到首页',
     toggleDarkMode: '切换夜间模式'
