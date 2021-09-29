4aeda465bf0946691c1b7161a602273768429a79
PengPengQ <57583682+PengPeng-Qi@users.noreply.github.com>
2021-09-29 22:48:01 +0800

    Update

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,62 @@
+module.exports = {
+  lang: 'zh-CN',
+  title: 'PengPengQ 的博客',
+  description: '个人博客 漆鹏 PengPengQ 碰碰漆',
+  base: '/blog/',
+
+  themeConfig: {
+    navbar: [{
+        text: '笔记',
+        link: '/notes/',
+      },
+      {
+        text: '项目',
+        link: '/projects/',
+      },
+
+    ],
+    sidebar: {
+      '/notes/': [{
+          text: 'HTML',
+          link: '/notes/HTML/',
+        }, {
+          text: 'CSS',
+          link: '/notes/CSS/',
+        }, {
+          text: 'JavaScript',
+          link: '/notes/JavaScript/',
+        }, {
+          text: 'Vue',
+          link: '/notes/Vue/',
+        }, {
+          text: 'React',
+          link: '/notes/React/',
+        }, {
+          text: '数据结构',
+          link: '/notes/数据结构/',
+        },
+        {
+          text: '其他',
+          children: [{
+            text: 'echo 命令',
+            link: '/notes/Others/echo',
+          }],
+        }
+      ],
+      '/projects/': [{
+        text: 'Reference',
+      }, ],
+    },
+    editLink: true,
+    /*     
+      editLinkText: '如果发现有误，可以点我进行编辑哦',
+      docsRepo: 'https://github.com/PengPeng-Qi',
+      docsBranch: 'master',
+      docsDir: 'docs',
+      editLinkPattern: ':repo/-/edit/:branch/:path',
+    */
+    logo: '/images/favicon.png',
+    backToHome: '别走丢了哦，点我回到首页',
+    toggleDarkMode: '切换夜间模式'
+  },
+}
---
+++ docs/README.md
@@ -0,0 +1,13 @@
+---
+home: true
+heroText: '欢迎来到我的博客'
+tagline: '这是我记录📝 学习笔记的地方'
+footer: <a href='https://github.com/PengPeng-Qi'>GitHub</a><br /><a href='https://weibo.com/u/6573456814'>微博</a><br /><a href='https://www.zhihu.com/people/peng-peng-qi-92'>知乎</a>
+footerHtml: true
+---
+## PengPengQ 的博客
+我是一名前端开发工程师，主要记录JavaScript、Vue、React、数据结构等，希望在我的博客里能找到你需要的知识，一起进步。
+
+这是我的第二个博客，很多文章还需要时间来迁移，正在一步一步完善之中，感谢你的关注
+
+查看笔记请点击右侧小飞机&nbsp;&nbsp;&nbsp; <a href='./notes/README.md' style='font-size: 20px'>✈️</a>
