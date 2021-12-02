899ee773197180bf6dcb72da28ed4d5c11a78a71
PengPengQ <1747447034@qq.com>
2021-12-02 15:30:11 +0800

    Update Hooks

--- docs/notes/React/Hooks/useEffect的基本使用.jsx
+++ docs/notes/React/Hooks/useEffect的基本使用.jsx
@@ -7,7 +7,7 @@ export default function App() {
   /* 
     相当于componentDidMount + componentDidUpdate
       有两个参数，不指定第二个参数的时候两个钩子🪝都会发生 
-      如果第二个参数为[]，则表示不监测任何值，表示只在相componentDidMount发生改变
+      如果第二个参数为[]，则表示不监测任何值，表示只在componentDidMount发生改变
       如果第二个参数为[number]，则表示监测number，只在初次挂载时和number改变时调用
     第一个参数返回的函数相当于componentWillUnmount
       每次执行useEffect的时候会先执行上一个useEffect第一个参数的返回值，一般做一些清除类的逻辑
