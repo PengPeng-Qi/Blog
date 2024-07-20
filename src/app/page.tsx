aa6bf1754b04ca07c154ee11a72b2975d0bd9131
pengpengq <1747447034@qq.com>
2024-07-20 21:38:47 +0800

    feat: add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <div className="w-screen h-16 flex justify-between items-center px-20 ">
-        logo
-        <div className="">menu</div>
-      </div>
+    <div className="w-screen h-14 flex justify-between items-center px-32">
+      <Link href={"/"} cursor-pointer>
+        <Image
+          src="/logo.svg"
+          alt="logo"
+          width={36}
+          height={36}
+          className="dark:invert"
+          priority
+        />
+      </Link>
 
-      <div></div>
-    </>
+      <div className="flex w-60 justify-between cursor-pointer">
+        <Link href={"/article"}>Article</Link>
+        <Link href={"/about"}>About</Link>
+        <Link href={"/projects"}>Projects</Link>
+      </div>
+    </div>
   );
 }
