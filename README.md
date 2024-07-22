9964c8500d8f7ed7af7745a5f14e71f804220c30
pengpengq <1747447034@qq.com>
2024-07-23 00:22:44 +0800

    feat: add gfm

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # To Do Lists
 
-- `tailwindcss` 顺序
+- [x] `tailwindcss` 顺序
+- [x] 强化 `Markdown` 语法 Gfm
+
+- `mdx` 使用 `@next/mdx/remote`
 - `mdx` 代码高亮+复制
 - `To Do` 页面开发
 - `SEO` 优化
-- 强化 `Markdown`（github）
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,13 +1,21 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
+import remarkGfm from "remark-gfm";
 import getAllBlogs from "../lib/blogs";
 
 export default async function Page() {
   const res = await getAllBlogs();
 
+  // 支持 Gfm 语法
+  const options = {
+    mdxOptions: {
+      remarkPlugins: [remarkGfm],
+    },
+  };
+
   return (
     <div className="mx-32">
       <article className="prose dark:prose-invert">
-        <MDXRemote source={res.content} />
+        <MDXRemote source={res.content} options={options} />
       </article>
     </div>
   );
