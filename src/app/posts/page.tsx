9e0145284e8774a7bd9c01c2aea6ef1d0a46b2ce
pengpengq <1747447034@qq.com>
2024-07-22 12:21:33 +0800

    refactor: update code

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -5,7 +5,7 @@ import Content from "./how-to-create-a-npm-package.mdx";
 export default function Page() {
   return (
     <div className="mx-32">
-      <Content></Content>
+      <Content />
     </div>
   );
 }
