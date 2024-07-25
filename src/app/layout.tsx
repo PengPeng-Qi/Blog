610c83ad0ccbe653f08df69de28d21f95ad32302
pengpengq <1747447034@qq.com>
2024-07-25 20:44:43 +0800

    feat: add web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "./components/Header";
@@ -26,6 +27,7 @@ export default function RootLayout({
         <Header />
 
         <div>{children}</div>
+        <Analytics />
       </body>
     </html>
   );
