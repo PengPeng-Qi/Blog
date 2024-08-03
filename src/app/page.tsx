04f4dada6331c818cc6f1eb29fe52f48d8bf02d3
pengpengq <1747447034@qq.com>
2024-08-03 18:21:34 +0800

    feat: update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex cursor-pointer justify-between">
       <div></div>
 
-      <div>
+      <div className="hidden xl:block">
         <div className="font-medium text-red-600">TOP CATEGORIES</div>
         <Tags />
       </div>
