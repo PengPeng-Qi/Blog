0eab9b055cd79ce6d067207df89f421dc46e0263
pengpengq <1747447034@qq.com>
2024-08-07 00:23:15 +0800

    feat: update search

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,14 +1,22 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import { useEffect, useState } from "react";
 import { initializeSearch, searchBlogs } from "../lib/search";
 import { Blog } from "../types/blogs";
 import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 
 export default function Search() {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState<Blog[]>([]);
-
-  const [showDialog, setShowDialog] = useState(false);
 
   useEffect(() => {
     initializeSearch();
@@ -27,31 +35,42 @@ export default function Search() {
 
   return (
     <div className="select-none">
-      <div
-        onClick={() => {
-          setShowDialog((value) => !value);
-        }}
-      >
-        <Button variant="ghost" size="sm">
-          <MagnifyingGlassIcon />
-        </Button>
-      </div>
-      {showDialog && (
-          <input
-            type="text"
-            value={query}
-            onChange={handleSearch}
-            placeholder="Search posts"
-          />
-        ) && (
-          <ul>
-            {results.map((post) => (
-              <li key={post.slug}>
-                <a href={`/article/${post.slug}`}>{post.title}</a>
-              </li>
-            ))}
-          </ul>
-        )}
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button
+            variant="outline"
+            size="sm"
+            className="w-56 dark:hover:bg-black"
+          >
+            <span>Search ...</span>
+          </Button>
+        </DialogTrigger>
+
+        <DialogContent className="p-0 sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle className="flex items-center justify-center px-3">
+              <MagnifyingGlassIcon className="mr-2 h-5 w-5" />
+              <Input
+                type="text"
+                value={query}
+                onChange={handleSearch}
+                placeholder="Search posts"
+                className="my-3 h-6 border-0"
+                autoFocus
+              />
+            </DialogTitle>
+            <DialogDescription>
+              <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+                {results.map((post) => (
+                  <li key={post.slug}>
+                    <a href={`/article/${post.slug}`}>{post.title}</a>
+                  </li>
+                ))}
+              </ul>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
