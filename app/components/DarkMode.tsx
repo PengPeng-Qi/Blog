fd8dd53241d401b00db384c52222d52361fff4bc
pengpengq <1747447034@qq.com>
2024-08-10 21:39:13 +0800

    feat: update dark mode

--- app/components/DarkMode.tsx
+++ app/components/DarkMode.tsx
@@ -10,27 +10,22 @@ type radioType = "dark" | "light" | "system";
 
 export default function DarkMode() {
   const Theme = useTheme();
-
-  const [radio, setRadio] = useState<radioType>(
-    localStorage.getItem("theme") as radioType,
-  );
+  const [radio, setRadio] = useState<radioType | undefined>(undefined);
 
   useEffect(() => {
-    const storedTheme = window.localStorage.getItem("theme");
+    const storedTheme = window.localStorage.getItem("theme") as radioType;
 
     if (storedTheme) {
-      if (storedTheme === "dark") {
-        setRadio("dark");
-      } else if (storedTheme === "light") {
-        setRadio("light");
-      } else {
-        setRadio("system");
-      }
+      setRadio(storedTheme);
+    } else {
+      setRadio("system");
     }
   }, []);
 
   useEffect(() => {
-    Theme.setTheme(radio);
+    if (radio) {
+      Theme.setTheme(radio);
+    }
   }, [radio, Theme]);
 
   return (
@@ -38,29 +33,39 @@ export default function DarkMode() {
       <div className="mx-3 flex justify-center rounded-full border border-gray-400 align-middle">
         <RadioGroup
           value={radio}
-          onValueChange={(value) =>
-            setRadio(value as "dark" | "light" | "system")
-          }
+          onValueChange={(value) => setRadio(value as radioType)}
           className="flex items-center justify-center"
         >
           <RadioGroupItem value="system" id="r1" />
           <Label htmlFor="r1">
             <GearIcon
-              className={`h-8 w-8 cursor-pointer rounded-full border p-2 hover:text-gray-950 dark:hover:text-gray-50 ${radio === "system" ? "border-gray-300 text-gray-950 dark:text-gray-50" : "border-transparent"}`}
+              className={`h-8 w-8 cursor-pointer rounded-full border p-2 hover:text-gray-950 dark:hover:text-gray-50 ${
+                radio === "system"
+                  ? "border-gray-300 text-gray-950 dark:text-gray-50"
+                  : "border-transparent"
+              }`}
             />
           </Label>
 
           <RadioGroupItem value="light" id="r2" />
           <Label htmlFor="r2">
             <SunIcon
-              className={`h-8 w-8 cursor-pointer rounded-full border p-2 hover:text-gray-950 dark:hover:text-gray-50 ${radio === "light" ? "border-gray-500 text-gray-950" : "border-transparent"}`}
+              className={`h-8 w-8 cursor-pointer rounded-full border p-2 hover:text-gray-950 dark:hover:text-gray-50 ${
+                radio === "light"
+                  ? "border-gray-500 text-gray-950"
+                  : "border-transparent"
+              }`}
             />
           </Label>
 
           <RadioGroupItem value="dark" id="r3" />
           <Label htmlFor="r3">
             <MoonIcon
-              className={`h-8 w-8 cursor-pointer rounded-full border p-2 hover:text-gray-950 dark:hover:text-gray-50 ${radio === "dark" ? "border-gray-300 text-gray-50" : "border-transparent"}`}
+              className={`h-8 w-8 cursor-pointer rounded-full border p-2 hover:text-gray-950 dark:hover:text-gray-50 ${
+                radio === "dark"
+                  ? "border-gray-300 text-gray-50"
+                  : "border-transparent"
+              }`}
             />
           </Label>
         </RadioGroup>
