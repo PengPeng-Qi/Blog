6d050ed6e04c475da8ab34c84076991474ce392f
pengpengq <1747447034@qq.com>
2024-08-10 20:35:19 +0800

    feat: update dark mode

--- app/components/DarkMode.tsx
+++ app/components/DarkMode.tsx
@@ -1,47 +1,70 @@
 "use client";
-import { motion } from "framer-motion";
+
+import { GearIcon, MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { RadioGroup, RadioGroupItem } from "@radix-ui/react-radio-group";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import { Switch } from "./ui/switch";
+import { Label } from "./ui/label";
+
+type radioType = "dark" | "light" | "system";
 
 export default function DarkMode() {
   const Theme = useTheme();
-  const [isChecked, setIsChecked] = useState<boolean | null>(null);
+
+  const [radio, setRadio] = useState<radioType>(
+    localStorage.getItem("theme") as radioType,
+  );
 
   useEffect(() => {
-    // 使用 resolvedTheme 初始化状态
     const storedTheme = window.localStorage.getItem("theme");
 
     if (storedTheme) {
-      setIsChecked(storedTheme === "dark");
-    } else {
-      setIsChecked(Theme.resolvedTheme === "dark");
+      if (storedTheme === "dark") {
+        setRadio("dark");
+      } else if (storedTheme === "light") {
+        setRadio("light");
+      } else {
+        setRadio("system");
+      }
     }
-  }, [Theme.resolvedTheme]);
+  }, []);
 
-  const handleClickDarkMode = (value: boolean) => {
-    setIsChecked(value);
-    Theme.setTheme(value ? "dark" : "light");
-  };
+  useEffect(() => {
+    Theme.setTheme(radio);
+  }, [radio, Theme]);
 
   return (
-    <div className="mx-3 flex items-center">
-      {isChecked === null ? (
-        <div className="w-11"></div>
-      ) : (
-        <motion.div
-          className="flex items-center"
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            duration: 0.8,
-            delay: 0.5,
-            ease: [0, 0.71, 0.2, 1.01],
-          }}
+    <div className="hidden sm:block">
+      <div className="mx-3 flex justify-center rounded-full border border-gray-500 align-middle">
+        <RadioGroup
+          value={radio}
+          onValueChange={(value) =>
+            setRadio(value as "dark" | "light" | "system")
+          }
+          className="flex items-center justify-center"
         >
-          <Switch checked={isChecked} onCheckedChange={handleClickDarkMode} />
-        </motion.div>
-      )}
+          <RadioGroupItem value="system" id="r1" />
+          <Label htmlFor="r1">
+            <GearIcon
+              className={`h-8 w-8 cursor-pointer rounded-full border p-2 hover:text-gray-950 dark:hover:text-gray-50 ${radio === "system" ? "border-gray-300 text-gray-950 dark:text-gray-50" : "border-transparent"}`}
+            />
+          </Label>
+
+          <RadioGroupItem value="light" id="r2" />
+          <Label htmlFor="r2">
+            <SunIcon
+              className={`h-8 w-8 cursor-pointer rounded-full border p-2 hover:text-gray-950 dark:hover:text-gray-50 ${radio === "light" ? "border-gray-500 text-gray-950" : "border-transparent"}`}
+            />
+          </Label>
+
+          <RadioGroupItem value="dark" id="r3" />
+          <Label htmlFor="r3">
+            <MoonIcon
+              className={`h-8 w-8 cursor-pointer rounded-full border p-2 hover:text-gray-950 dark:hover:text-gray-50 ${radio === "dark" ? "border-gray-300 text-gray-50" : "border-transparent"}`}
+            />
+          </Label>
+        </RadioGroup>
+      </div>
     </div>
   );
 }
