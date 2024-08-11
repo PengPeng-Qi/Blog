937221b1b4c84b8f990378a3b0d68bc320eaea65
pengpengq <1747447034@qq.com>
2024-08-12 00:01:39 +0800

    feat: update demo icon

--- app/components/header/DemoIcon.tsx
+++ app/components/header/DemoIcon.tsx
@@ -7,7 +7,7 @@ export default function GitHubIcon() {
       <Link href="/demos" className="">
         <CubeIcon className="h-5 w-5 text-gray-400 hover:text-gray-950 dark:hover:text-gray-50" />
       </Link>
-      <div className="absolute left-2 top-8 hidden p-2 text-xs group-hover:block">
+      <div className="absolute right-2 top-8 hidden p-2 text-xs group-hover:block sm:left-2">
         Demos
       </div>
     </div>
