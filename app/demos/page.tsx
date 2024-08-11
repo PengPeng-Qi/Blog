f06609f6f473902567abcaf2e99a44e868200123
pengpengq <1747447034@qq.com>
2024-08-12 00:49:32 +0800

    feat: add demos link

--- app/demos/page.tsx
+++ app/demos/page.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 export default function Demo() {
   return (
     <div className="cursor-pointer">
-      <div className="mb-6 font-medium text-red-600">100 Days Demo</div>
+      <div className="mb-6 font-medium text-red-600">
+        <Link
+          href="https://pengpeng-qi.github.io/100-days-demo/"
+          target="_blank"
+        >
+          100 Days Demo
+        </Link>
+      </div>
 
       <div className="mb-3 flex leading-none hover:text-light-primary dark:hover:text-dark-primary">
         <span className="font-medium">Dark Mode</span>
