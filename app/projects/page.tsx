2f8ffc7656fcf4d2e404e1d597f42a2910cff936
pengpengq <1747447034@qq.com>
2024-08-12 00:07:04 +0800

    feat: update projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,17 @@
 export default function Page() {
-  return <>projects</>;
+  return (
+    <div className="cursor-pointer">
+      <div className="mb-6 font-medium text-red-600">Tools</div>
+
+      <div className="mb-3 flex leading-none hover:text-light-primary dark:hover:text-dark-primary">
+        <span className="font-medium">Prettier Config</span>
+      </div>
+      <div className="mb-3 flex leading-none hover:text-light-primary dark:hover:text-dark-primary">
+        <span className="font-medium">NextJs Template</span>
+      </div>
+      <div className="mb-3 flex leading-none hover:text-light-primary dark:hover:text-dark-primary">
+        <span className="font-medium">VSCode Extension</span>
+      </div>
+    </div>
+  );
 }
