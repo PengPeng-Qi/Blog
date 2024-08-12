89955bda49bc259f3fc487a57bf5e322f4cca605
pengpengq <1747447034@qq.com>
2024-08-12 18:28:50 +0800

    feat: update demo link

--- app/demos/layout.tsx
+++
@@ -1,5 +0,0 @@
-export default function DemoLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return <div>{children}</div>;
-}
--- components/header/DemoIcon.tsx
+++ components/header/DemoIcon.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function GitHubIcon() {
   return (
     <div className="group relative ml-4 flex items-center">
-      <Link href="/demos" className="">
+      <Link href="https://pengpeng-qi.github.io/100-days-demo/" target="_blank">
         <CubeIcon className="h-5 w-5 text-gray-400 hover:text-gray-950 dark:hover:text-gray-50" />
       </Link>
       <div className="absolute right-2 top-8 hidden p-2 text-xs group-hover:block sm:left-2">
