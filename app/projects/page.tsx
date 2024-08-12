c40f3d7558bdb4973ab3eee97d29ccca38901823
pengpengq <1747447034@qq.com>
2024-08-12 14:09:57 +0800

    feat: update project

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,19 +6,28 @@ export default function Page() {
       <div className="mb-6 font-medium text-red-600">Tools</div>
 
       <div className="mb-3 flex leading-none hover:text-light-primary dark:hover:text-dark-primary">
-        <span className="font-medium">Prettier Config</span>
+        <Link
+          href="https://www.npmjs.com/package/coder-prettier-config"
+          target="_blank"
+        >
+          <span className="font-medium">Prettier Config</span>
+        </Link>
       </div>
       <div className="mb-3 flex leading-none hover:text-light-primary dark:hover:text-dark-primary">
         <Link
           href="https://pengpeng-qi.github.io/nextjs-tailwind-starter-template/"
           target="_blank"
         >
-          Next.js Tailwind Starter Template
+          <span className="font-medium">Next.js Tailwind Starter Template</span>
         </Link>
-        <span className="font-medium"></span>
       </div>
       <div className="mb-3 flex leading-none hover:text-light-primary dark:hover:text-dark-primary">
-        <span className="font-medium">VSCode Extension</span>
+        <div className="relative">
+          <span className="font-medium">VSCode Extension</span>
+          <span className="absolute -right-9 -top-2 rounded-full border border-gray-400 px-1 text-xs text-gray-400">
+            to do
+          </span>
+        </div>
       </div>
     </div>
   );
