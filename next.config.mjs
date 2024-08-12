0dabe865f1543a2899985c168b51e537771a1fc2
pengpengq <1747447034@qq.com>
2024-08-12 21:35:31 +0800

    feat: add code inspector

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,18 @@
 import withMDX from "@next/mdx";
+import { codeInspectorPlugin } from "code-inspector-plugin";
+
+const withMDXConfig = withMDX({
+  extension: /\.mdx?$/,
+  options: {},
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  webpack: (config, { dev, isServer }) => {
+    config.plugins.push(codeInspectorPlugin({ bundler: "webpack" }));
+    return config;
+  },
 };
 
-// Wrap MDX and Next.js config with each other
-export default withMDX(nextConfig);
+export default withMDXConfig(nextConfig);
