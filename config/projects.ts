ca8f5e167c063b15e6b7b73d11049b7759d8a5d0
pengpengq <1747447034@qq.com>
2024-08-19 14:51:06 +0800

    feat: add chrome extension

--- config/projects.ts
+++ config/projects.ts
@@ -5,4 +5,8 @@ export const projectsStructure = [
     link: "https://pengpeng-qi.github.io/nextjs-tailwind-starter-template/",
   },
   { label: "100 Days Demo", link: "https://pengpeng-qi.github.io/100-days-demo/" },
+  {
+    label: "WeiBo Chrome Extension",
+    link: "https://github.com/PengPeng-Qi/demo_monorepo_management/tree/master/packages/weibo-chrome-extension",
+  },
 ];
