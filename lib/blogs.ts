2bbce2d68880dfb4177782a634f9856c5ce23a7a
pengpengq <1747447034@qq.com>
2024-08-21 17:37:58 +0800

    refactor: update blog lib

--- lib/blogs.ts
+++ lib/blogs.ts
@@ -5,16 +5,21 @@ import matter from "gray-matter";
 import path from "path";
 
 const blogsDirectory = path.join(process.cwd(), "blogs");
+const fileNamesArr = await fs.promises.readdir(blogsDirectory);
+
+// 获取每个博客的 Content
+const getFileContent = async (filename: string) => {
+  const fullPath = path.join(blogsDirectory, filename);
+  const fileContents = await fs.promises.readFile(fullPath, "utf8");
+
+  return matter(fileContents);
+};
 
 export async function getAllBlogs() {
-  const fileNamesArr = await fs.promises.readdir(blogsDirectory);
-
   const blogs: Blogs = await Promise.all(
     fileNamesArr.map(async (filename) => {
-      const fullPath = path.join(blogsDirectory, filename);
-      const fileContents = await fs.promises.readFile(fullPath, "utf8");
-      // 解析 mdx 内的 yaml 文本
-      const { data, content } = matter(fileContents);
+      const { data, content } = await getFileContent(filename);
+
       return {
         metadata: data,
         title: data.title,
@@ -32,20 +37,16 @@ export async function getAllBlogs() {
 
 export async function generateSearchIndex() {
   const blogs = await getAllBlogs();
-
   fs.writeFileSync("public/searchIndex.json", JSON.stringify(blogs));
 }
 
+// 通过 slug 获取文章内容
 export async function getCurBlog(slug: string) {
-  const fileNamesArr = await fs.promises.readdir(blogsDirectory);
-
   const blog: Blog | null = await Promise.race(
     fileNamesArr.map(async (filename) => {
-      const fullPath = path.join(blogsDirectory, filename);
-      const fileContents = await fs.promises.readFile(fullPath, "utf8");
-      const { data, content } = matter(fileContents);
+      const { data, content } = await getFileContent(filename);
 
-      if (data.slug === slug) {
+      if (data.slug === slug && data.isPublish !== false) {
         return Promise.resolve({
           metadata: data,
           title: data.title,
@@ -67,47 +68,41 @@ export async function getCurBlog(slug: string) {
   return blog;
 }
 
+// 获取所有的 tag 信息，并返回降序排列的 tag 数组
 export async function getAllTags() {
-  const blogsDirectory = path.join(process.cwd(), "blogs");
-  const fileNamesArr = await fs.promises.readdir(blogsDirectory);
-
-  const tags: string[] = [];
+  const tagsWithCountMap = new Map<string, number>();
 
   await Promise.all(
     fileNamesArr.map(async (filename) => {
-      const fullPath = path.join(blogsDirectory, filename);
-      const fileContents = await fs.promises.readFile(fullPath, "utf8");
-      // 解析 mdx 内的 yaml 文本
-      const { data } = matter(fileContents);
-
+      const { data } = await getFileContent(filename);
       if (data.isPublish === false) return;
 
       data.tag.forEach((item: string) => {
         item = TransformString(item);
 
-        if (!tags.includes(item)) tags.push(item);
+        if (!tagsWithCountMap.has(item)) {
+          tagsWithCountMap.set(item, 0);
+        }
+        tagsWithCountMap.set(item, (tagsWithCountMap.get(item) as number) + 1);
       });
     })
   );
 
-  tags.sort();
+  // 按出现次数降序排列
+  const sortedTags = Array.from(tagsWithCountMap.entries())
+    .sort((a, b) => b[1] - a[1])
+    .map((key) => key[0]);
 
-  return tags;
+  return sortedTags;
 }
 
+// 通过 tag 查找相关的一系列文章
 export async function getAllBlogsByTag(tag: string) {
-  const fileNamesArr = await fs.promises.readdir(blogsDirectory);
-
   const blogs: Blogs = [];
 
   await Promise.all(
     fileNamesArr.map(async (filename) => {
-      const fullPath = path.join(blogsDirectory, filename);
-      const fileContents = await fs.promises.readFile(fullPath, "utf8");
-      // 解析 mdx 内的 yaml 文本
-      const { data, content } = matter(fileContents);
-
-      if (data.isPublish === false) return;
+      const { data, content } = await getFileContent(filename);
 
       data.tag.forEach((item: string) => {
         item = TransformString(item);
@@ -119,6 +114,7 @@ export async function getAllBlogsByTag(tag: string) {
             slug: data.slug,
             createdTime: data.createdTime,
             modifiedTime: data.modifiedTime,
+            isPublish: data.isPublish ?? true,
             content,
           });
         }
@@ -126,5 +122,5 @@ export async function getAllBlogsByTag(tag: string) {
     })
   );
 
-  return blogs;
+  return blogs.filter((blog) => blog.isPublish);
 }
